9a351f967854f198ac2bcbef315d29d26f36eb85
khairul <khairulice@gmail.com>
2018-08-11 15:06:00 +0600

    testing

--- user.router.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET ALL UserS */
-router.get('/', function(req, res, next) {
-   res.send("Hello API");
-  });
-
-  module.exports = router;
